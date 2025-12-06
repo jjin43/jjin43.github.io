2989a4a836d09422d76fd4ebdaa39f6db7f3b370
Justin Jin <justinjinaz@gmail.com>
2025-12-06 02:28:13 -0700

    init

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# jjin43.github.io
-### [Visit Page](https://jjin43.github.io/)
-Yet Another Personal Website - Justin Jin / jjin43
+# [jjin43.github.io](https://jjin43.github.io/)
+Personal Website - Justin Jin / jjin43
+
+## Installation 
+Download the project to your computer and install needed packages with command:
+
+```bash
+$ npm install
+```
+Then create .env file:
+```bash
+SITE_URL='https://example.com' ## Required to generate sitemap by next-sitemap 
+RESEND_API_KEY='your_resend_api_key' ## Required to send email by resend
+```
+
+Then start the project on the local server with the command:
+
+```bash
+$ npm run dev
+```
+
+and open http://localhost:3000.
+
+## Tech Stack
+- Next.js
+- TypeScript
+- Tailwind CSS
+- Framer-motion
+- React-hook-form
+- Resend
---
+++ src/components/about.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { SectionHeading } from '@/components/section-heading';
+import { Skills } from '@/components/skills';
+import { useSectionInView } from '@/hooks/use-section-in-view';
+
+export const About = () => {
+  const { ref } = useSectionInView('About');
+
+  return (
+    <motion.section
+      ref={ref}
+      id="about"
+      className="my-10 flex w-full scroll-mt-28 flex-col items-center md:mb-20"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading heading="About Me" />
+      <div className="-mt-5 max-w-2xl text-center leading-7">
+        <p className="mb-4">
+          I&apos;m Justin Jin, I&apos;m a fullstack developer. I&apos;m always
+          looking to learn new technologies.
+        </p>
+        <p>
+          I&apos;m open to Job opportunities where I can contribute, learn and
+          grow. If you have a good opportunity that matches my skills and
+          experience then don&apos;t hesitate to contact me.
+        </p>
+      </div>
+      <Skills />
+    </motion.section>
+  );
+};

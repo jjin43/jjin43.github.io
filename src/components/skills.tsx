e165d7b073143f926adf814398fe8cddfbf3277d
Justin Jin <justinjinaz@gmail.com>
2025-12-08 03:29:13 -0700

    feat: skills label

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -21,7 +21,7 @@ const fadeInAnimationVariants = {
 export const Skills = () => {
   return (
     <div className="mt-10 flex w-full flex-wrap justify-between gap-10 px-5 sm:justify-center sm:px-0 md:mt-14 lg:justify-between">
-      {skillsData.map(({ icon }, index) => (
+      {skillsData.map(({ icon, name }, index) => (
         <motion.div
           key={index}
           variants={fadeInAnimationVariants}
@@ -31,8 +31,12 @@ export const Skills = () => {
             once: true,
           }}
           custom={index}
+          className="flex flex-col items-center gap-2"
         >
           {icon}
+          <span className="text-muted-foreground text-sm font-medium">
+            {name}
+          </span>
         </motion.div>
       ))}
     </div>
